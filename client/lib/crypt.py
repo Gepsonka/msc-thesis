dabd130c742b6a5f4b948d50a1251ff4377c2880
Molnár Botond <45335158+Gepsonka@users.noreply.github.com>
2025-03-14 23:53:22 +0100

    fizes

--- client/lib/crypt.py
+++ client/lib/crypt.py
@@ -9,12 +9,39 @@ import ecdsa.ellipticcurve
 
 CURVE = ecdsa.SECP256k1
 
+def string_to_bigint(s):
+    byte_array = s.encode('utf-8')  
+    big_int = int.from_bytes(byte_array, byteorder='big')
+    return big_int
+
+def bigint_to_string(n):
+    byte_array = n.to_bytes((n.bit_length() + 7) // 8, byteorder='big')
+    return byte_array.decode('utf-8')
+
+
+def bytes_to_bigint(b):
+    return int.from_bytes(b, byteorder='big')
+
+def bigint_to_bytes(n, length = 64):
+    return n.to_bytes(length, byteorder='big')
+
+def sha256_int(n):
+    return hashlib.sha256(n.to_bytes((n.bit_length() + 7) // 8, byteorder='big')).digest()
+def xor_bytes(bytes1, bytes2):
+    if len(bytes1) > len(bytes2):
+        return bytes(bytes1[i] ^ bytes2[i % len(bytes2)] for i in range(len(bytes1)))
+    if len(bytes2) > len(bytes1):
+        return bytes(bytes2[i] ^ bytes1[i % len(bytes1)] for i in range(len(bytes2)))
+    return bytes(a ^ b for a, b in zip(bytes1, bytes2))
+    
 def generate_keys():
   private_key = ecdsa.SigningKey.generate(curve=CURVE)
-  public_key: ecdsa.VerifyingKey = private_key.get_verifying_key()
+  public_key= private_key.get_verifying_key()
+
+  return private_key.privkey.secret_multiplier, public_key.pubkey.point
 
 
-  return private_key, public_key
+
 
 
 def read_keys_from_hex(private_key_text):
@@ -27,7 +54,7 @@ def read_keys_from_hex(private_key_text):
 
 
 
-def calculate_chameleon_hash(m0, r0, x: ecdsa.SigningKey) -> ecdsa.ellipticcurve.PointJacobi:
+def calculate_chameleon_hash(m0: int, r0: int, x: int) -> ecdsa.ellipticcurve.PointJacobi:
   '''
   Calculates chameleon hash from the given parameters
 
@@ -39,8 +66,7 @@ def calculate_chameleon_hash(m0, r0, x: ecdsa.SigningKey) -> ecdsa.ellipticcurve
   Returns:
   Chameleon hash
   '''
-  privkey_int = x.privkey.secret_multiplier 
-  point = (m0 + r0 * privkey_int) * CURVE.generator
+  point = (m0 + r0 * x) * CURVE.generator
   return point
 
 
@@ -50,44 +76,27 @@ def calculate_message_format(m: bytes):
   m: message in utf-8 string format
 
   Returns:
-  Message bytes in the format: m || timestamp || H(m || timestamp)
+  Message int in the format: m || timestamp || H(m || timestamp)
   '''
-  timestamp = int(time.time())
-
-  timestamp_byte_length = (timestamp.bit_length() + 7) // 8 or 1
-  timestamp_bytes = timestamp.to_bytes(timestamp_byte_length, 'big')
+  timestamp_bytes = str(int(time.time())).encode()
 
   timestamp_message_hash = hashlib.sha256(m + timestamp_bytes).digest()
 
   return m + timestamp_bytes + timestamp_message_hash
 
-def m_encryption(m_: bytes, client_private_key: ecdsa.SigningKey, server_public_key: ecdsa.VerifyingKey):
-  '''
-  Parameters:
-  m_: message in the format returner by calculate_message_format
-  private_key: Client private key
-  server_public_key: Public key from the server
+def encrypt_m(m: int, delta: int, common_point: ecdsa.ellipticcurve.PointJacobi): 
+  delta_hash = sha256_int(delta)
 
-  Returns:
-  Encrypted message
-  '''
-  random_delta = random.getrandbits(256)
-  random_delta_bytes = random_delta.to_bytes(32, 'big')
-  hashed_delta_bytes = hashlib.sha256(random_delta_bytes).digest()
+  m_send = bytes_to_bigint(xor_bytes(delta_hash, bigint_to_bytes(m)))
 
-  message_bytes = m_
+  x_common = bigint_to_bytes(common_point.x(), 32)
+  delta_bytes = bigint_to_bytes(delta, 32)
 
-  cipher = AES.new(hashed_delta_bytes, AES.MODE_ECB)
-  padded_data = pad(message_bytes, AES.block_size)
-  ciphertext = cipher.encrypt(padded_data)
+  delta_xor_commonkey = xor_bytes(delta_bytes, x_common)
 
-  common_public_key = server_public_key.pubkey.point * client_private_key.privkey.secret_multiplier
-  common_public_key_x_bytes = common_public_key.x().to_bytes(32, "big")
-  common_public_key_delta_xor = bytes(a ^ b for a, b in zip(random_delta_bytes, common_public_key_x_bytes))
+  return m_send, delta_xor_commonkey
 
-  return ciphertext + common_public_key_delta_xor
-  
-def r_encryption(converted_message: bytes, m0: int, r0: int, client_private_key: ecdsa.SigningKey):
+def encrypt_r(m: int, m0: int, r0: int, client_private_key: int, private_key_mult_inverse: int):
   '''
   Creates r__ in the protocol
 
@@ -100,18 +109,8 @@ def r_encryption(converted_message: bytes, m0: int, r0: int, client_private_key:
   Returns:
   bytes: Encrypted r
   '''
-  converted_message_int = int.from_bytes(converted_message, 'big')
 
-  m_minus_m0 = converted_message_int - m0
-
-  private_key_mult_inverse = pow(client_private_key.privkey.secret_multiplier, -1, CURVE.order)
-
-  r__ = r0 - m_minus_m0 * private_key_mult_inverse
-
-  r__byte_length = (r__.bit_length() + 7) // 8 or 1 
-  r__bytes = r__.to_bytes(r__byte_length, byteorder='big', signed=True)
-
-  return r__bytes
+  return (r0 - (m-m0) * private_key_mult_inverse) % CURVE.order
 
 
 def chameleon_hash_challange(m__: bytes, r__: bytes, client_public_key: ecdsa.VerifyingKey, chameleon_hash: ecdsa.ellipticcurve.PointJacobi) -> bool:
